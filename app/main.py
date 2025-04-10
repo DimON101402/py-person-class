492043491a87b9ee0130fb19ba8a86513ff1ed1e
Dimon <kosma4evskuj@gmail.com>
2025-04-10 20:18:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,8 +1,31 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        Person.people[name] = self
+
+    def set_spouse(self, role, spouse_name):
+        if spouse_name in Person.people:
+            spouse = Person.people[spouse_name]
+            setattr(self, role, spouse)
+            reverse_role = "husband" if role == "wife" else "wife"
+            setattr(spouse, reverse_role, self)
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people):
+    [Person(p["name"], p["age"]) for p in people]
+
+    person_list = []
+    for person_dict in people:
+        person = Person.people[person_dict["name"]]
+
+        if person_dict.get("wife"):
+            person.set_spouse("wife", person_dict["wife"])
+        elif person_dict.get("husband"):
+            person.set_spouse("husband", person_dict["husband"])
+
+        person_list.append(person)
+
+    return person_list
